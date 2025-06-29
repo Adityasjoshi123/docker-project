6453683d3c97ce02feaf73b2648c10c2dbee3c88
Ubuntu <ubuntu@ip-172-20-1-237.ap-south-1.compute.internal>
2025-06-29 12:04:46 +0000

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Dockerized App');
+  res.end('Hello from Dockerized App,Aditya is the goat');
 });
 
 server.listen(8080, () => {
